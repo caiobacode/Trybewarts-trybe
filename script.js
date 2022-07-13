9ac996372e261cfdce8593d62e5cb7dca2319a87
Robson <rm8l@outlook.com>
2022-07-13 17:54:56 -0300

    correcao identacao

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const button = document.getElementById('botao')
-button.addEventListener('click', function(event) {
-  event.preventDefault()
-  const email = document.getElementById('email')
-  const senha = document.getElementById('password')
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    return window.alert('Olá, Tryber!')
-  }
-  else {
-    return window.alert('Email ou senha inválidos.')
-  }
+button.addEventListener('click', function (event) {
+	event.preventDefault()
+	const email = document.getElementById('email')
+	const senha = document.getElementById('password')
+	if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+		return window.alert('Olá, Tryber!')
+	}
+	else {
+		return window.alert('Email ou senha inválidos.')
+	}
 })
