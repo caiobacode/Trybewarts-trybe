f277b41c88cefdc1658712beda0a12c5856bb8bb
caiobacode <caioodsf@gmail.com>
2022-07-13 16:41:09 -0300

    javascript

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let email = document.getElementById('email')
-let senha = document.getElementById('senha')
-let button = document.getElementById('botao')
-
+const button = document.getElementById('botao')
 button.addEventListener('click', function() {
-    
+  const email = document.getElementById('email')
+  const senha = document.getElementById('password')
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!')
+  }
+  else {
+    alert('Email ou senha inválidos.')
+  }
 })
 
