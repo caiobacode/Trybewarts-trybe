1def4ee211a7f4e95c56405324009ca269edd921
Robson <rm8l@outlook.com>
2022-07-14 03:42:52 -0300

    correção linter

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const button = document.getElementById('botao')
-button.addEventListener('click', function (event) {
-	event.preventDefault()
-	const email = document.getElementById('email')
-	const senha = document.getElementById('password')
-	if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-		return window.alert('Olá, Tryber!')
-	}
-	else {
-		return window.alert('Email ou senha inválidos.')
-	}
-})
+const button = document.getElementById('botao');
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const email = document.getElementById('email');
+  const senha = document.getElementById('password');
+  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+    return window.alert('Olá, Tryber!');
+  }
+
+  return window.alert('Email ou senha inválidos.');
+});
